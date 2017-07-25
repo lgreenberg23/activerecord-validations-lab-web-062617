be15e3cf7d9271e1af318b43a51d24ccd6536136
github username <github email address>
2017-07-25 13:37:01 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+	validates :name, presence: true
+	validates :name, length: { minimum: 2 }
+	validates :name, uniqueness: true
+	validates :phone_number, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+	
+
+	validates :title, presence: true
+	validates :content, length: { minimum: 250 }
+	validates :summary, length: { maximum: 250 }
+	validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+	validate :validate_clickbait, if: :title
+
+	def validate_clickbait
+    	if ["Won't Believe", "Secret", "Top", "Guess"].none? {|phrase| self.title.include?(phrase)}
+  			errors.add(:title, "make your title more annoying")
+  		end
+  	end
+
 end
+
